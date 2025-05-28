d0bb8f5bd6a980ce27e77ba864728089ce11e78a
Andrei Loginov <andorus911@gmail.com>
2025-05-28 19:04:01 +0300

    add homehandler

--- unit-converter/main.go
+++ unit-converter/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
 
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", HomeHandler)
+
+	http.ListenAndServe(":8080", nil)
 }
