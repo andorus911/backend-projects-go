13c794fff6d80bd96c72f306ccd8fc68670db566
Andrei Loginov <andorus911@gmail.com>
2025-05-28 20:03:33 +0300

    add ConvertHandler

--- unit-converter/main.go
+++ unit-converter/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
 )
+
+type ConversionRequest struct {
+	Value float64 `json:"value"`
+	From  string  `json:"from"`
+	To    string  `json:"to"`
+}
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("index.html"))
@@ -14,8 +21,30 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func ConvertHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if ct := r.Header.Get("Content-Type"); ct != "application/json" {
+		http.Error(w, "Expected JSON", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	var req ConversionRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	log.Print()
+}
+
 func main() {
 	http.HandleFunc("/", HomeHandler)
+
+	http.HandleFunc("/api/convert", ConvertHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
